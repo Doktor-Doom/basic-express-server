9fbfc580ba1544cff6c624e21df3bfd4f68153ef
Doktor-Doom <andre.o.martin@gmail.com>
2020-12-08 22:57:25 -0800

    initial placeholder

---
+++ error-handling/404.js
@@ -0,0 +1,7 @@
+'use strict';
+function notFoundHandler(req, res, next){
+  res.status(404);
+  res.statusMessage = 'Resource Not Found';
+  res.json({ error: 'not found' });
+}
+module.exports = notFoundHandler;
